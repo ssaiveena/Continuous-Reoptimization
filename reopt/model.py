3e1608762155bf515c4b99bc4f8a19a28d81fd82
Sai Veena <40389169+ssaiveena@users.noreply.github.com>
2023-04-13 14:07:11 -0700

    Add files via upload

---
+++ reopt/model.py
@@ -0,0 +1,262 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from numba import njit
+from util import *
+
+'''
+SSJRB simulation model. Three parts:
+Reservoir releases, gains (into Delta), and Delta pumping
+Each component has two functions: step() and fit()
+Then all components are combined in the simulate() function
+Numba compilation requires only numpy arrays, no pandas objects
+'''
+
+@njit
+def get_tocs(x, d):
+  tp = [0, x[1], x[2], x[3], 366]
+  sp = [1, x[4], x[4], 1, 1]
+  return np.interp(d, tp, sp)
+
+@njit 
+def reservoir_step(x, dowy, Q, S, K, R_avg, S_avg, tocs):
+  '''
+  Advances reservoir storage from one timestep to the next
+
+    Parameters:
+      x (np.array): Reservoir rule parameters (5)
+      dowy (int): Day of water year
+      Q (float): Inflow, cfs
+      S (float): Storage, acre-feet
+      K (float): Storage capacity, acre-feet
+      R_avg (float): Median release for this day of the year, cfs
+      S_avg (float): Median storage for this day of the year, acre-feet
+      tocs (float): Top of conservation storage, fraction of capacity
+
+    Returns:
+      tuple(float, float): the updated release (cfs) and storage (af)
+
+  '''
+  R_avg *= cfs_to_afd
+  Q *= cfs_to_afd
+
+  R_target = R_avg # default
+
+  if S < S_avg:
+    R_target = R_avg * (S / S_avg) ** x[0] # exponential hedging
+   
+  S_target = max(0,S + Q - R_target) # assumes 1-day forecast
+
+  if S_target > K * tocs: # flood pool
+    R_target += (S_target - K * tocs) * x[5]
+    S_target = max(0,S + Q - R_target)
+
+  if S_target > K: # spill
+    R_target += S_target - K
+  elif S_target < K * x[6]: # below dead pool
+    R_target = max(0, R_target-(K * x[6] - S_target))
+
+  return (R_target * afd_to_cfs, np.max(np.array([S + Q - R_target, 0])))
+
+
+@njit
+def gains_step(x, dowy, Q_total, Q_total_avg, S_total_pct, Gains_avg):
+  '''
+  Compute gains into the Delta for one timestep
+
+    Parameters:
+      x (np.array): Gains parameters 
+      dowy (int): Day of water year
+      Q_total (float): Total inflow to all reservoirs, cfs
+      Q_total_avg (float): Average total inflow for this day of the year, cfs
+      S_total_pct (float): System-wide reservoir storage, % of median
+      Gains_avg (float): Median gains for this day of the year, cfs
+
+    Returns:
+      (float): Gains into the Delta for one timestep, cfs
+
+  '''
+  G = Gains_avg * S_total_pct ** x[0] # adjust up/down for wet/dry conditions
+  if Q_total > x[1] * Q_total_avg: # high inflows correlated with other tributaries
+    G += Q_total * x[2]
+  return G
+
+
+@njit
+def gains_fit(x, dowy, Q_total, Q_total_avg, S_total_pct, Gains_avg, Gains_obs):
+  '''
+  Evaluate Delta gains model against historical observations for a set of parameters
+
+    Parameters:
+      x (np.array): Gains parameters
+      dowy (np.array(int)): Day of water year
+      Q_total (np.array(float)): Total inflow to all reservoirs, cfs
+      Q_total_avg (np.array(float)): Average total inflow 
+                                     for this day of the year, cfs
+      S_total_pct (np.array(float)): System-wide reservoir storage, % of median
+      Gains_avg (np.array(float)): Median gains for each day of the year, cfs
+      Gains_obs (np.array(float)): Observed historical gains, cfs
+
+    Returns:
+      (float): the negative r**2 value of Delta gains, to be minimized
+
+  '''
+  T = dowy.size
+  G = np.zeros(T)
+
+  for t in range(T):
+    inputs = (dowy[t], Q_total[t], Q_total_avg[dowy[t]], S_total_pct[t], Gains_avg[dowy[t]])
+    G[t] = gains_step(x, *inputs)
+
+  return -np.corrcoef(Gains_obs, G)[0,1]**2
+
+
+@njit
+def pump_step(x, dowy, Q_in, Kp, Pump_pct_avg, Pump_avg, S_total_pct):
+  '''
+  Compute Delta pumping for one timestep
+
+    Parameters:
+      x (np.array): Delta pumping parameters
+      dowy (int): Day of water year
+      Q_in (float): Total inflow to the Delta, cfs 
+                    (sum of all reservoir outflows plus gains)
+      Kp (float): Pump capacity, cfs
+      Pump_pct_avg (float): (Average pumping / Average inflow) 
+                    for this day of the year, unitless
+      S_total_pct (float): System-wide reservoir storage, % of median
+
+    Returns:
+      tuple(float, float): Pumping (cfs) and outflow (cfs)
+
+  '''
+
+  Q_in = np.max(np.array([Q_in, 0.0]))
+
+  export_ratio = x[5] if dowy < 273 else x[6]
+  outflow_req = x[3] if dowy < 273 else x[4]
+
+  P = Q_in * Pump_pct_avg * S_total_pct ** x[0] # ~ export ratio
+
+  if Q_in < x[1]: # approximate dry year adjustment
+    P *= np.min(np.array([S_total_pct, 1.0])) ** x[2]
+
+  if P > Q_in * export_ratio:
+    P = Q_in * export_ratio
+  if P > Q_in - outflow_req:
+    P = np.max(np.array([Q_in - outflow_req, 0]))
+
+  if 182 <= dowy <= 242: # env rule apr-may
+    Kp = 750
+  
+  P = np.min(np.array([P, Kp]))
+  return P
+
+
+@njit
+def pump_fit(x, dowy, Q_in, Kp, Pump_pct_avg, Pump_cfs_avg, S_total_pct, Pump_obs):
+  '''
+  Evaluate pump policy against historical observations for a set of parameters
+
+    Parameters:
+      x (np.array): Delta pumping parameter (1)
+      dowy (np.array(int)): Day of water year
+      Q_in (np.array(float)): Total inflow to the Delta, cfs 
+                              (sum of all reservoir outflows plus gains)
+      Kp (float): Pump capacity, cfs
+      Pump_pct_avg (np.array(float)): (Average pumping / Average inflow) 
+                                      for each day of the year, unitless
+      S_total_pct (np.array(float)): System-wide reservoir storage, % of median
+      Pump_obs (np.array(float)): Observed historical pumping, cfs
+
+    Returns:
+      (float): the negative r**2 value to be minimized
+
+  '''
+  T = dowy.size
+  P = np.zeros(T)
+
+  for t in range(T):
+    inputs = (dowy[t], Q_in[t], Kp, Pump_pct_avg[dowy[t]], Pump_cfs_avg[dowy[t]], S_total_pct[t])
+    P[t] = pump_step(x, *inputs)
+
+  return -np.corrcoef(Pump_obs, P)[0,1]**2
+
+
+def simulate(params, Kr, Kp, safecap, dowy, Q, Q_avg, R_avg, S_avg, Gains_avg, Pump_pct_avg, Pump_avg, DM, S0):
+  '''
+  Run full system simulation over a given time period.
+
+    Parameters:
+      params (tuple(np.array)): Parameter arrays for all reservoirs, gains, and Delta
+      Kr (np.array(float)): Reservoir capacities, af
+      Kp (np.array(float)): Pump capacities, cfs
+      dowy (np.array(int)): Day of water year
+      Q (np.array(float, float)): Matrix of inflows at all reservoirs, cfs
+      Q_avg (np.array(float, float)): Matrix of median inflows for each reservoir
+                                      for each day of the year, cfs
+      R_avg (np.array(float, float)): Matrix of median releases for each reservoir
+                                      for each day of the year, cfs
+      S_avg (np.array(float, float)): Matrix of median storage for each reservoir
+                                      for each day of the year, af
+      Gains_avg (np.array(float)): Median gains for each day of the year, cfs
+      Pump_pct_avg (np.array(float, float)): (Median pumping / median inflow) 
+                                             for each day of the year for each reservoir, unitless
+      Pump_avg (np.array(float, float)): Median pumping for each day of the year
+                                         for each reservoir, cfs
+      DM (np.array(float)): Demand multiplier, system-wide, unitless
+                            (=1.0 for the historical scenario)
+      S0 (np.array(float)): Initial storage for each reservoir, af
+
+    Returns:
+      (tuple(np.array, np.array, np.array)): Matrices of timeseries results (cfs) 
+        for reservoir releases, storage, and Delta Gains/Inflow/Pumping/Outflow
+
+  '''  
+  cfs_to_taf = 2.29568411 * 10 ** -5 * 86400 / 1000
+
+  T,NR = Q.shape # timesteps, reservoirs
+  NP = 2 # pumps
+  R,S,G,I,P, storage_cost, flood_cost, shortage_cost = (np.zeros((T,NR)), np.zeros((T,NR)),
+               np.zeros(T), np.zeros(T), np.zeros((T,NP)), np.zeros((T,NR)), np.zeros((T,NR)), np.zeros((T,NR)))
+
+  Q_total = Q.sum(axis=1)
+  Q_total_avg = Q_avg.sum(axis=1)
+  S_total_avg = S_avg.sum(axis=1)
+
+  tocs = np.zeros((T,NR))
+  for r in range(NR):
+    tocs[:,r] = get_tocs(params[r], dowy)
+  
+  for t in range(0,T):
+    d = dowy[t]
+
+    # 1. Reservoir policies
+    for r in range(NR):
+      res_demand = R_avg[d,r] * DM[t] # median historical release * demand multiplier
+      if t == 0:
+        inputs_r = (d, Q[t,r], S0[r], Kr[r], res_demand, S_avg[d,r], tocs[t,r])
+      else:
+        inputs_r = (d, Q[t, r], S[t - 1, r], Kr[r], res_demand, S_avg[d, r], tocs[t, r])
+
+      R[t,r], S[t,r] = reservoir_step(params[r], *inputs_r)
+
+      shortage_cost[t, r] = max(res_demand - R[t, r], 0) ** 2 
+      if R[t, r] > safecap[r]:
+        # flood penalty, high enough to be a constraint
+        flood_cost[t, r] += 10 ** 5 * (R[t, r] - safecap[r])
+    # 2. Gains into Delta
+    S_total_pct = S[t].sum() / S_total_avg[d]
+    inputs_g = (d, Q_total[t], Q_total_avg[d], 
+              S_total_pct, np.min(np.array([Gains_avg[d] * DM[t], Gains_avg[d]])))
+    G[t] = gains_step(params[NR], *inputs_g)
+
+    # 3. Delta pumping policies
+    I[t] = R[t].sum() + G[t]
+    for p in range(NP):
+      if p == 0: S_total_pct = S[t,1] / S_avg[d,1] # SWP - ORO only
+      inputs_p = (dowy[t], I[t], Kp[p], Pump_pct_avg[d,p], Pump_avg[d,p], S_total_pct)
+      P[t,p] = pump_step(params[NR+p+1], *inputs_p)
+
+  Delta = np.vstack((G,I,P[:,0],P[:,1],I-P.sum(axis=1))).T # Gains, Inflow, Pumping, Outflow
+  
+  return (R, S, Delta, shortage_cost, flood_cost)
---
+++ reopt/setup_Future.py
@@ -0,0 +1,192 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+import model
+from scipy.optimize import differential_evolution as DE
+from util import *
+import time
+from numba import njit
+import math
+
+# functions to pull numpy arrays from dataframes
+
+def reservoir_training_data(k, v, df, medians, df_demand, init_storage=False):
+    dowy = df.dowy.values
+    Q = df[k + '_inflow_cfs'].values
+    K = v['capacity_taf'] * 1000
+    # R_obs = df[k+'_outflow_cfs'].values
+    # S_obs = df[k+'_storage_af'].values if K > 0 else np.zeros(dowy.size)
+    Q_avg = medians[k + '_inflow_cfs'].values
+    R_avg = medians[k + '_outflow_cfs'].values
+    S_avg = medians[k + '_storage_af'].values if K > 0 else np.zeros(dowy.size)
+    R_max = v['safe_release_cfs']
+    if not init_storage:
+        S0 = S_avg[-1]
+    else:
+        S0 = df[k + '_storage_af'].values[-1]
+
+    return (dowy, Q, K, R_avg, S_avg, S0, R_max, df_demand.combined_demand.values)  # deleted R_obs after Q_avg
+
+def reservoir_training_data_updated(k, v, df, medians, df_demand, df_opt, init_storage=False):
+    dowy = df.dowy.values
+    Q = df[k + '_inflow_cfs'].values
+    K = v['capacity_taf'] * 1000
+    # R_obs = df[k+'_outflow_cfs'].values
+    # S_obs = df[k+'_storage_af'].values if K > 0 else np.zeros(dowy.size)
+    Q_avg = medians[k + '_inflow_cfs'].values
+    R_avg = medians[k + '_outflow_cfs'].values
+    S_avg = medians[k + '_storage_af'].values if K > 0 else np.zeros(dowy.size)
+    R_max = v['safe_release_cfs']
+    if not init_storage:
+        S0 = S_avg[-1]
+    else:
+        S0 = df_opt[k + '_storage_af'].values[-1]
+        
+    return (dowy, Q, K, R_avg, S_avg, S0, R_max, df_demand.combined_demand.values)  # deleted R_obs after Q_avg
+
+# pull numpy arrays from dataframes
+def get_simulation_data(res_keys, pump_keys, df_hydrology, medians, df_demand=None, init_storage=False):
+    dowy = df_hydrology.dowy.values
+    Q = df_hydrology[[k + '_inflow_cfs' for k in res_keys]].values
+    Q_avg = medians[[k + '_inflow_cfs' for k in res_keys]].values
+    R_avg = medians[[k + '_outflow_cfs' for k in res_keys]].values
+    S_avg = medians[[k + '_storage_af' for k in res_keys]].values
+    Gains_avg = medians['delta_gains_cfs'].values
+    Pump_pct_avg = medians[[k + '_pumping_pct' for k in pump_keys]].values
+    Pump_cfs_avg = medians[[k + '_pumping_cfs' for k in pump_keys]].values
+
+    if df_demand is None:
+        demand_multiplier = np.ones(dowy.size)
+    else:
+        demand_multiplier = df_demand.combined_demand.values
+
+    if not init_storage:
+        S0 = S_avg[-1, :]
+    else:
+        S0 = df_hydrology[[k + '_storage_af' for k in res_keys]].values[-1, :]
+
+    return (dowy, Q, Q_avg, R_avg, S_avg, Gains_avg, Pump_pct_avg, Pump_cfs_avg, demand_multiplier, S0)
+
+# pull numpy arrays from dataframes
+def get_simulation_data_updated(res_keys, pump_keys, df_hydrology, medians, df_opt, df_demand=None, init_storage=False):
+    dowy = df_hydrology.dowy.values
+    Q = df_hydrology[[k + '_inflow_cfs' for k in res_keys]].values
+    Q_avg = medians[[k + '_inflow_cfs' for k in res_keys]].values
+    R_avg = medians[[k + '_outflow_cfs' for k in res_keys]].values
+    S_avg = medians[[k + '_storage_af' for k in res_keys]].values
+    Gains_avg = medians['delta_gains_cfs'].values
+    Pump_pct_avg = medians[[k + '_pumping_pct' for k in pump_keys]].values
+    Pump_cfs_avg = medians[[k + '_pumping_cfs' for k in pump_keys]].values
+
+    if df_demand is None:
+        demand_multiplier = np.ones(dowy.size)
+    else:
+        demand_multiplier = df_demand.combined_demand.values
+
+    if not init_storage:
+        S0 = S_avg[-1, :]
+    else:
+        S0 = df_opt[[k + '_storage_af' for k in res_keys]].values[-1, :]
+
+    return (dowy, Q, Q_avg, R_avg, S_avg, Gains_avg, Pump_pct_avg, Pump_cfs_avg, demand_multiplier, S0)
+
+
+@njit
+def get_tocs(x, d):
+    tp = [0, x[1], x[2], x[3], 366]
+    sp = [1, x[4], x[4], 1, 1]
+    return np.interp(d, tp, sp)
+
+
+@njit
+def reservoir_step(x, dowy, Q, S, K, R_avg, S_avg, tocs):
+    '''
+  Advances reservoir storage from one timestep to the next
+
+    Parameters:
+      x (np.array): Reservoir rule parameters (5)
+      dowy (int): Day of water year
+      Q (float): Inflow, cfs
+      S (float): Storage, acre-feet
+      K (float): Storage capacity, acre-feet
+      R_avg (float): Median release for this day of the year, cfs
+      S_avg (float): Median storage for this day of the year, acre-feet
+      tocs (float): Top of conservation storage, fraction of capacity
+
+    Returns:
+      tuple(float, float): the updated release (cfs) and storage (af)
+
+  '''
+    R_avg *= cfs_to_afd
+    Q *= cfs_to_afd
+
+    R_target = R_avg  # default
+
+    if S < S_avg:
+        R_target = R_avg * (S / S_avg) ** x[0]  # exponential hedging
+
+    S_target = max(0, S + Q - R_target)  # assumes 1-day forecast
+
+    if S_target > K * tocs:  # flood pool
+        R_target += (S_target - K * tocs) * x[5]
+        S_target = max(0, S + Q - R_target)
+
+    if S_target > K:  # spill
+        R_target += S_target - K
+    elif S_target < K * x[6]:  # below dead pool
+        R_target = max(0,R_target - (K * x[6] - S_target))
+
+    return (R_target * afd_to_cfs, np.max(np.array([S + Q - R_target, 0])))
+
+
+cfs_to_taf = 2.29568411 * 10 ** -5 * 86400 / 1000
+
+
+@njit
+def reservoir_fit(x, dowy, Q, K, R_avg, S_avg, S0, R_max, df_demand):  # removed R_obs after R_avg
+    """
+    Evaluate reservoir model against historical observations for a set of parameters
+
+      Parameters:
+        x (np.array): Reservoir rule parameters (5)
+        dowy (np.array(int)): Day of water year over the simulation
+        Q (np.array(float)): Inflow, cfs
+        S (np.array(float)): Storage, acre-feet
+        K (float): Storage capacity, acre-feet
+        R_avg (np.array(float)): Median release for each day of the year, cfs
+        R_obs (np.array(float)): Observed historical release, cfs
+        S_avg (np.array(float)): Median storage for each day of the year, acre-feet
+        S_obs (np.array(float)): Observed historical storage, acre-feet.
+          Not used currently, but could fit parameters to this instead.
+        S0 (float): initial storage, acre-feet
+
+      Returns:
+        (float): the negative r**2 value of reservoir releases, to be minimized
+  """
+    tt = dowy.size
+    R, S, res_demand = np.zeros(tt), np.zeros(tt), np.zeros(tt)
+    tocs = get_tocs(x, dowy)
+    shortage_cost, flood_cost,storage_cost = [np.zeros(tt) for _ in range(3)]
+    # D = R_obs #demand is assumed as observed historical release, cfs
+    DM = df_demand
+
+    for t in range(0, tt):
+        d = dowy[t]
+        res_demand[t] = R_avg[d] * DM[t]  # median historical release * demand multiplier
+        if t==0:
+          inputs_r = (d, Q[t], S0, K, res_demand[t], S_avg[dowy[t - 1]], tocs[t])
+        else:
+          inputs_r = (d, Q[t], S[t - 1], K, res_demand[t], S_avg[dowy[t - 1]], tocs[t])
+        
+        R[t], S[t] = reservoir_step(x, *inputs_r)
+
+        # squared deficit.
+        # should be able to vectorize this.
+        shortage_cost[t] = max(res_demand[t] - R[t], 0) ** 2 
+
+        if R[t] > R_max:
+            # flood penalty, high enough to be a constraint
+            flood_cost[t] += 10 ** 5 * (R[t] - R_max)
+
+    return shortage_cost.sum() + flood_cost.sum() 
